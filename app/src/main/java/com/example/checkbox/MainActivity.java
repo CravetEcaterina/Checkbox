4331298925c121ce0c9c416af6dcde0ed3c414f2
Cravet Ecaterina <kravetkatea@gmail.com>
2024-02-07 11:27:32 +0200

    замена цвета

--- app/src/main/java/com/example/checkbox/MainActivity.java
+++ app/src/main/java/com/example/checkbox/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.checkbox;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.checkboxes.R;
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
@@ -10,6 +11,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     CheckBox M, F;
+    View rootView;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         if(checkBox.isChecked()) {
             selection.setText("Был выбран Mужской пол");
             F.setChecked(false);
+            rootView.setBackgroundColor(Color.parseColor("#568dfc"));
         }
     }
     public void on_F_Checked(View view) {
@@ -32,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         if(checkBox.isChecked()) {
             selection.setText("Был выбран Женский пол");
             M.setChecked(false);
+            rootView.setBackgroundColor(Color.parseColor("#ff6b6b"));
         }
     }
 }
